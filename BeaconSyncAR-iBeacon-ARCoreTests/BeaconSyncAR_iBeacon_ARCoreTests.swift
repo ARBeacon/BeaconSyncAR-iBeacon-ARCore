a880ff89296c01918dde598e5291533b3b1f7432
Matthew <h.mattjunk@gmail.com>
2024-11-02 16:42:03 +0800

    Initial Commit

---
+++ BeaconSyncAR-iBeacon-ARCoreTests/BeaconSyncAR_iBeacon_ARCoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  BeaconSyncAR_iBeacon_ARCoreTests.swift
+//  BeaconSyncAR-iBeacon-ARCoreTests
+//
+//  Created by Maitree Hirunteeyakul on 11/2/24.
+//
+
+import Testing
+@testable import BeaconSyncAR_iBeacon_ARCore
+
+struct BeaconSyncAR_iBeacon_ARCoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
